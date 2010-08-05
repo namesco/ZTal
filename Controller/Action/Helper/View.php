ca68dcf74df0352e895a9fb690a5d43b8c361304
Alex Mace <alex@hollytree.co.uk>
2010-08-05 20:12:08 +0100

    ZTal will now find templates if you are not explicitly using modules in your application

--- Controller/Action/Helper/View.php
+++ Controller/Action/Helper/View.php
@@ -32,9 +32,19 @@ class Ztal_Controller_Action_Helper_View extends Zend_Controller_Action_Helper_A
 		//add a path to search for templates based on the current module 
 		$module = $this->_actionController->getRequest()->getModuleName();
 		$phptal = $this->_actionController->view->getEngine();
-		$phptal->setTemplateRepository(SITE_ROOT_PATH . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR
-			. ($module != '' ? 'modules' . DIRECTORY_SEPARATOR . $module : '') . DIRECTORY_SEPARATOR . 'views'
-			. DIRECTORY_SEPARATOR . 'scripts');
+		$phptal->setTemplateRepository(
+			SITE_ROOT_PATH . DIRECTORY_SEPARATOR . 'application' .
+			DIRECTORY_SEPARATOR .
+			($module != '' ? 'modules' . DIRECTORY_SEPARATOR . $module : '') .
+			DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'scripts');
+
+		// If the module name is 'default', then the application may not
+		// actually be using modules, so we need to also look in the views
+		// directory in the application directory.
+		$phptal->setTemplateRepository(
+			SITE_ROOT_PATH . DIRECTORY_SEPARATOR . 'application' .
+			DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'scripts');
+
 			
 		$translator = $phptal->getTranslator();
 		if (is_object($translator)) {
